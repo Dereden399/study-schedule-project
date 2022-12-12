93064aa8e8cf1d387c27e980856811bd24ab5963
denis <denis.kuznetsov@aalto.fi>
2022-12-12 16:56:50 +0200

    express server set up. db structure plan

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ A small fullstack web application to plan a study schedule
 
 ## Technologies used
 1. Express typescript
+
+## Database structure
+### User
+1. username
+2. password
+3. schedule
+### Schedule
+1. course list
+2. name
+3. description
+### Course
+1. name
+2. starting date
+3. end date
+4. additional info
+
--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,16 @@
-console.log("hello");
+import express from "express";
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/ping", (_req, res) => {
+    console.log("pinged");
+    res.send("pong");
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}.`);
+});
